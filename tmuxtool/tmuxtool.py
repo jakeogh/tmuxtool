d5aa19a495e94e7234710b4e69d33ca276dd0c1a
jakeogh <github.com@v6y.net>
2022-02-13 15:34:19 -0700

    auto-commit

--- tmuxtool/tmuxtool.py
+++ tmuxtool/tmuxtool.py
@@ -91,7 +91,7 @@ def list_tmux(*,
 def get_server_pids():
     server_pids = []
     for proc in psutil.process_iter(['pid', 'name', 'username', 'open_files']):
-        if proc.info['name'].startswith('tmux'):
+        if proc.info['name'] == 'tmux: server':
             print(proc.info)
             server_pids.append(proc.info['pid'])
 
